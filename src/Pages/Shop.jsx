0895a4d2df2c39aafc76188b6cfeea780520fe71
yashika jain <jainyashika532@gmail.com>
2024-06-02 16:57:34 +0530

    nav bar created

---
+++ src/Pages/Shop.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hero from '../Components/Hero/Hero'
+import Navbar from '../Components/Navbar/Navbar'
+import './Shop.css'
+const Shop = () => {
+  return (
+    <div className='main-container'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Shop
